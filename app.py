1e543388045f75a78d596283c3cf6aec53554b20
Nicolas Kivimäki <nicolaskivimaki@Emmas-MacBook-Pro.local>
2022-09-25 19:50:59 +0300

    updates

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
-    db.session.commit()
-    result = db.session.execute("SELECT COUNT(*) FROM visitors")
-    counter = result.fetchone()[0]
-    return render_template("index.html", counter=counter)
+import routes
--- schema.sql
+++ schema.sql
@@ -1,1 +1,11 @@
-CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP
+);
