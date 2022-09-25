ebac08b23df9592dca59ea5c94cecd21daf56ac5
Nicolas Kivimäki <nicolaskivimaki@Emmas-MacBook-Pro.local>
2022-09-25 21:08:03 +0300

    users

--- routes.py
+++ routes.py
@@ -6,14 +6,29 @@ import users
 def index():
     return render_template("index.html")
 
-@app.route("/login",methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-	if request.method == "GET":
-		return render_template("login.html")
-	if request.method == "POST":
-		username = request.form["username"]
-		password = request.form["password"]
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
 
-		if not users.login(username, password):
-			return render_template("error.html", message="Väärä tunnus tai salasana!")
-		return redirect("/")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
--- users.py
+++ users.py
@@ -4,35 +4,31 @@ from flask import abort, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username, password):
-    sql = "SELECT password, id, role FROM users WHERE username=:username"
+    sql = "SELECT id, password FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
         return False
-    if not check_password_hash(user[0], password):
-        return False
-    session["user_id"] = user[1]
-    session["username"] = username
-    session["user_role"] = user[2]
-    session["csrf_token"] = os.urandom(16).hex()
-    return True
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def user_id():
+    return session.get("user_id", 0)
 
 def logout():
     del session["user_id"]
-    del session["username"]
-    del session["user_role"]
 
-def register(username, password, role):
+def register(username, password):
     hash_value = generate_password_hash(password)
     try:
-        sql= """INSERT INTO users (username, password, role)
-                VALUES (:username, :password, :role)"""
-        db.session.execute(sql, {"username":username, "password":hash_value, "role":role})
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
         db.session.commit()
     except:
         return False
     return login(username, password)
 
-def check_csfr():
-    if session["csfr_token"] != request.form["csfr_token"]:
-        abort(403)
